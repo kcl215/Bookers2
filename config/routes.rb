7eae03db04fb438f68fc645b11b62762ee229b63
EC2 Default User <ec2-user@ip-172-31-25-149.ec2.internal>
2022-02-14 09:23:42 +0000

    [ADD]bootstrapを導入

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+  get 'users/index'
+  get 'users/show'
+  get 'users/edit'
+  resources :books
+  devise_for :users
   root to: "homes#top"
+  get '/home/about' => "homes#about" , as: 'about'
+
 end
